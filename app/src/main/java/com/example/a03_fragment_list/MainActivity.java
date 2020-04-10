891d1b8fc3f2ac0a103775a38d0650344c740b40
AlmudenaRA <almudena.ruedas@gmail.com>
2020-04-10 13:14:57 +0200

    Initial commit

---
+++ app/src/main/java/com/example/a03_fragment_list/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.a03_fragment_list;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.contenedor, AveriaFragment.newInstance(2))
+                .commit();
+    }
+}
